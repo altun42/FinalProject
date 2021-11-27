e3e714ed5c715c816cd53aa547391e46e3d71bc8
Kerim ALTUN <76987626+zigetvar@users.noreply.github.com>
2021-11-28 02:18:20 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+ProductManager productManager = new ProductManager(new EfProductDal());
+
+foreach (var product in productManager.GetByUnitPrice(40,100))
+{
+    Console.WriteLine(product.ProductName);
+
+}
